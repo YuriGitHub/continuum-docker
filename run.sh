57ed38abf068a6962624e08f843e232fb5317832
akash <Akash>
2018-08-10 18:23:10 +0530

    another fix to run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,7 @@
 SERVICE_CONFIG_FILE="/etc/continuum/service.conf"
 
 if [ -f "${SERVICE_CONFIG_FILE}" ]; then
-    if [ ! -z "${SERVICE}"]; then
         echo "service ${SERVICE}" >> ${SERVICE_CONFIG_FILE}
-    fi
 fi
 
 

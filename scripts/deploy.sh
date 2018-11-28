89ddf5898b1aa3a9e0297719775aeab913da3a8c
Alejandro Guzman <a.guillermo.guzman@gmail.com>
2018-11-28 15:55:21 -0500

    Update deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,41 +23,37 @@ prod_dockerfile=./prod/Dockerfile
 prod_dockercompose=./prod/docker-compose.yml
 
 # ##################
-# Get link
+# Get link/version
 # ##################
-if [[ -n $2 ]]; then
-    # If an installer link was passed in, we'll use that and get the version/tag from it
+
+# If an installer link was passed in, we'll use that and get the version/tag
+# from it, else let's search for it in prod/docker-compose.yml
+if [[ -n  $2 ]]; then
     installer_link=$2
+else
+    installer_link=$(grep -oP "(https.*installer\.sh)" ${prod_dockercompose} || true)
+fi
+
+# Find the version, strategy depends on if the installer link was found in
+# the environment
+if [[ -n "${installer_link}" ]]; then
     # Finds a feature branch
     version=$(grep -oP '(\d{2}\.\d\.\d*\.\d*-[S|D]-\d{5})' <<< ${installer_link} || true)
+
     if [[ -z ${version} ]]; then
         # Finds a dev branch
         version=$(grep -oP '(\d{2}\.\d\.\d*\.\d*)' <<< ${installer_link} || true)
     fi
+
     if [[ -z ${version} ]]; then
         echo "Installer link must be versioned"
         exit 1
     fi
 else
-    # Else, let's search for it in prod/docker-compose.yml (think of it like
-    # the same as getting it from the environment)
-    installer_link=$(grep -oP "(https.*installer\.sh)" ${prod_dockercompose} || true)
-    if [[ -n ${installer_link} ]]; then
-        version=$(grep -oP '(\d{2}\.\d\.\d*\.\d*-[S|D]-\d{5})' <<< ${installer_link} || true)
-        if [[ -z ${version} ]]; then
-            # Finds a dev branch
-            version=$(grep -oP '(\d{2}\.\d\.\d*\.\d*)' <<< ${installer_link} || true)
-        fi
-        if [[ -z ${version} ]]; then
-            echo "Installer link must be versioned"
-            exit 1
-        fi
-    else
-        # If we didn't find it there, we'll default to the last public version
-        # listed in the prod/Dockerfile itself
-        echo "Defaulting to installer in prod/Dockerfile"
-        version=$(grep -oP '(ENV CONTINUUM_VERSION .*)' ${prod_dockerfile} | cut -d " " -f 3)
-    fi
+    # If we didn't find it there, we'll default to the last public version
+    # listed in the prod/Dockerfile itself
+    echo "Defaulting to installer in prod/Dockerfile"
+    version=$(grep -oP '(ENV CONTINUUM_VERSION .*)' ${prod_dockerfile} | cut -d " " -f 3)
 fi
 
 # In any of the above cases we needed to extract a version/tag, if nothing was
@@ -72,6 +68,9 @@ tag=${version}-${build_id}
 # ############################
 # Build, tag, publish
 # ############################
+
+# if the installer_link is empty, build will default to installer version
+# installer hard coded in prod/Dockerfile
 docker image build --file ${prod_dockerfile} --tag continuum:prod --build-arg INSTALLER_LINK=${installer_link} --no-cache .
 docker image build --file ${ossum_dockerfile} --tag ${organization}/${image}:latest --no-cache .
 
